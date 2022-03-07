a6ed54a5f8b3a6446f6fa969e780b08e359cb551
Руслан <@WIN-F52HFKED8TO>
2022-03-07 21:20:01 +0300

    Добавьте файлы проекта.

---
+++ App5/App5/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CustomCalendarControl.Views;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCalendar_Clicked(object sender, EventArgs e)
+        {
+            var calendarPage = new CalendarPage(DateTime.Now, null);
+            calendarPage.SelectedDateCommand = new Command<DateTime>((item) =>
+            {
+                lblDate.Text = item.ToString("dd MMM yyyy");
+            });
+            await PopupNavigation.Instance.PushAsync(calendarPage);
+        }
+       
+    }
+}
